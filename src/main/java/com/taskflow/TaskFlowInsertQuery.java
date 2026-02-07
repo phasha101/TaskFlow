f24bf3d0fc7ceaefebf96715223269ac30d75adc
Karabo Phasha <mawaiskara@gmail.com>
2026-02-07 14:57:06 +0200

    Insert Sample Tasks

--- src/main/java/com/taskflow/TaskFlowInsertQuery.java
+++ src/main/java/com/taskflow/TaskFlowInsertQuery.java
@@ -7,11 +7,18 @@ public class TaskFlowInsertQuery {
         String url = "jdbc:postgresql://localhost:5432/tasks";
         String user = "postgres";
         String password = "0000";
+        String sql = "insert INTO task(id, title, priority, status, category) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection connection = DriverManager.getConnection(url, user, password);
-             Statement stmt = connection.createStatement()) {
-
+             PreparedStatement stmt = connection.prepareStatement(sql)) {
             System.out.println("connected to database");
+            stmt.setInt(1, 4);
+            stmt.setString(2, "dishes");
+            stmt.setInt(3, 2);
+            stmt.setString(4,"PENDING");
+            stmt.setString(5, "CHORES");
+            stmt.execute();
+            System.out.println("data entered");
 
 
         } catch (SQLException e) {
