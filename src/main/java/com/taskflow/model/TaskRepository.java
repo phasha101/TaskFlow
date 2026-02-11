7805473cd07735f566dacde952dc707cdde32770
Karabo Phasha <mawaiskara@gmail.com>
2026-02-11 23:56:33 +0200

    initialized crud methods

--- src/main/java/com/taskflow/model/TaskRepository.java
+++ src/main/java/com/taskflow/model/TaskRepository.java
@@ -1,4 +1,21 @@
 package com.taskflow.model;
 
+import org.hibernate.SessionFactory;
+
 public class TaskRepository {
+
+    HibernateUtil util = new HibernateUtil();
+//    SessionFactory sessionFactory = util.
+
+    public void save(Task task){
+
+    }
+
+    public void  findAll(){
+
+    }
+
+    public void delete(Task task){
+
+    }
 }
