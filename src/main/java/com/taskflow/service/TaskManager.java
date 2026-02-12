41d7c95cfdddc0e188be1b11b411d368282bd541
Karabo Phasha <mawaiskara@gmail.com>
2026-02-13 00:10:21 +0200

    refactored crud methods to use hibernate

--- src/main/java/com/taskflow/service/TaskManager.java
+++ src/main/java/com/taskflow/service/TaskManager.java
@@ -11,85 +11,89 @@ public class TaskManager {
 
     private List<Task> tasks = new ArrayList<>();
     private Priority priority = Priority.LOW;
-    String url = "jdbc:postgresql://localhost:5432/tasks";
-    String user = "postgres";
-    String password = "0000";
 
     TaskRepository taskRepository = new TaskRepository();
 
     public void createTask(String taskTitle, Category category, long daysToComplete, Priority priority) {
         Task task = new Task(taskTitle, category, daysToComplete, priority);
-        tasks.add(task);
+        taskRepository.save(task);
         System.out.println("Task added! on: " + LocalDate.now());
     }
 
     public void createTask(String taskTitle, Category category, long daysToComplete) {
 
         Task task = new Task(taskTitle, category, daysToComplete, this.priority);
-        tasks.add(task);
+        taskRepository.save(task);
         System.out.println("Task added! on: " + LocalDate.now());
     }
 
     public void updateTaskTitle(UUID id, String title) {
-        for (Task x : tasks) {
-            if (x.getID().equals(id)) {
-                x.setTitle(title);
-                System.out.println("Title change successful");
-                return;
-            }
+        Task task = taskRepository.findById(id);
+        if (task!=null){
+            task.setTitle(title);
+            taskRepository.save(task);
+            System.out.println("Title updated successfully");
         }
-        System.out.println("Task not found.");
+        else {
+            System.out.println("Task of id: " + id + " not found.");
+        }
     }
 
     public void updateTaskCategory(UUID id, Category category) {
-        for (Task x : tasks) {
-            if (x.getID().equals(id)) {
-                x.setCategory(category);
-                System.out.println("Category change successful");
-                return;
-            }
+        Task task = taskRepository.findById(id);
+        if (task!=null){
+            task.setCategory(category);
+            taskRepository.save(task);
+            System.out.println("category updated successfully");
         }
-        System.out.println("Task not found.");
+        else {
+            System.out.println("Task of id: " + id + " not found.");
+        }
     }
 
     public void updateTaskPriority(UUID id, Priority newPriority){
-        for (Task x : tasks) {
-            if (x.getID().equals(id)) {
-                x.setPriority(newPriority);
-                System.out.println("Priority change successful");
-                return;
-            }
+        Task task = taskRepository.findById(id);
+        if (task!=null){
+            task.setPriority(priority);
+            taskRepository.save(task);
+            System.out.println("priority updated successfully");
         }
-        System.out.println("Task not found.");
+        else {
+            System.out.println("Task of id: " + id + " not found.");
+        }
     }
 
 
     public void updateTaskDeadline(UUID id, LocalDate date) {
-        for (Task x : tasks) {
-            if (x.getID().equals(id)) {
-                x.setDeadline(date);
-                System.out.println("Deadline change successful");
-                return;
-            }
+        Task task = taskRepository.findById(id);
+        if (task!=null){
+            task.setDeadline(date);
+            taskRepository.save(task);
+            System.out.println("deadline updated successfully");
         }
-        System.out.println("Task not found.");
+        else {
+            System.out.println("Task of id: " + id + " not found.");
+        }
     }
 
     public void listTasks() {
-        if (tasks.isEmpty()) {
-            System.out.println("No tasks found.");
-        } else {
-            tasks.forEach(System.out::println);
+        List<Task> tasks = taskRepository.findAll();
+        for(Task task:tasks){
+            System.out.println(task);
         }
     }
 
     public void deleteTask(UUID id) {
-        boolean removed = tasks.removeIf(x -> x.getID().equals(id));
-        System.out.println(removed ? "Task of id: " + id + " removed" 
-                                   : "Failed to remove task of id: " + id);
+        try {
+            taskRepository.delete(id);
+        }catch (Exception e){
+            System.out.println("could not delete task of id: " + id + "\n" + e);
+        }
     }
 
-    public List<Task> getTasks() { return tasks; }
+    public List<Task> getTasks() {
+        return tasks;
+    }
 
     public void saveTasksToDB(List<Task> tasks) {
         for (Task task: tasks){
